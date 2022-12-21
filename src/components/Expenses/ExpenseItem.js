c543424efa830815d00baafbb1c04d59316884b2
chunjie26 <benderang26@gmail.com>
2022-12-21 22:23:58 +0800

    Add cardview component and explore children props

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,17 @@
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
 
 function ExpenseItem(props) {
 
     return ( // with react, we can put a {} in the jsx html tag inside to call the dynamic data to be shown
-        <div className='expense-item'>
+        <Card className='expense-item'>
             <ExpenseDate date = {props.date}/>
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
-        </div>
+        </Card>
     );
 }
 
