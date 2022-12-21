f8b49dd1be38051859ced75119a357432fec5bb3
chunjie26 <benderang26@gmail.com>
2022-12-21 21:47:24 +0800

    Extract Date become a component and make it display on ExpenseItem

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,11 @@
 import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem(props) {
-    const month = props.date.toLocaleString('en-US', {month: 'long'})
-    const day = props.date.toLocaleString('en-US', {day: '2-digit'})
-    const year = props.date.getFullYear();
-    
 
     return ( // with react, we can put a {} in the jsx html tag inside to call the dynamic data to be shown
         <div className='expense-item'>
-            <div>
-                <div>{month}</div>
-                <div>{day}</div>
-                <div>{year}</div>
-            </div> 
+            <ExpenseDate date = {props.date}/>
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
