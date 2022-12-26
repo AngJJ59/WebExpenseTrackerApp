abfb375fbd5c1be7221e037dd411e463c62e7fc2
chunjie26 <benderang26@gmail.com>
2022-12-26 16:47:05 +0800

    add button and evenet listener using useState

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,23 @@
+import React, { useState } from 'react'; // use state allows us to define values as state where changes to these values should show 
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
 function ExpenseItem(props) {
+    const [title, newTitle] = useState(props.title)
+
+    const clickHandler = () => {
+        newTitle('Green tea')
+    }
 
     return ( // with react, we can put a {} in the jsx html tag inside to call the dynamic data to be shown
         <Card className='expense-item'>
             <ExpenseDate date = {props.date}/>
             <div className='expense-item__description'>
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
+            <button onClick={clickHandler}>Change Title</button>
         </Card>
     );
 }
